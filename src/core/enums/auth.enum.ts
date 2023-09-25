8d755f1f7b2ceb0ff84c9999f9db0c7ebd29c920
CamiloAlejoT <camiloalejot94@gmail.com>
2023-09-24 21:56:56 -0500

    add DTO's to logIn and signIn

---
+++ src/core/enums/auth.enum.ts
@@ -0,0 +1,9 @@
+export enum AUTH {
+    EMAILINVALID = 'Invalid email format',
+    EMAILEMPTY = "Email is required",
+    PASSWORDNOTSTRING = "Password must be a string",
+    PASSWORDLENGTH = "Password must be at least 6 characters long",
+    PASSWORDEMPRY = "Password is required",
+    NAMENOTSTRING = "Name must be a string",
+    NAMEEMPTY = "Name is required"
+}
